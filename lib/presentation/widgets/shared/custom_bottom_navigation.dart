e2145fef43d6b097e869af3168012c7a93f6ce99
Juan Guachichullca <100295290+IgnacioJuan@users.noreply.github.com>
2024-10-05 02:56:22 -0500

    Navegacion manteniedo estados

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  final int currentIndex;
+
+  const CustomBottomNavigation({super.key, required this.currentIndex});
+
+  void onItemTap(BuildContext context, int index) {
+    context.go('/home/$index');
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      onTap: (value) => onItemTap(context, value),
       elevation: 0,
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
